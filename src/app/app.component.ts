de127c49894192e5e76ae878a6449b7d534538f1
DialloBarry <safietoudiallo9@gmail.com>
2021-03-13 20:25:26 +0100

    comprendre les binding angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'mon-premier-projet';
+  isAuth=false;
+  appareils=[
+    {name: 'machine a laver',
+    status:'allumé'
+  },
+  {name: 'tele',
+  status:'eteint'
+},
+{name: 'pc',
+status:'allumé'
+}
+
+  ]
+  /*appareil1: string="machine a lave";
+  appareil2:string="tvs";
+  appareil3:string="ordinateur";*/
+
+  constructor(){
+    setTimeout(() => { this.isAuth=true }, 4000);
+
+  }
+  onTurnOn(){
+    console.log('cest allumer');
+  }
 }
